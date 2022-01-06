9af2b11ee5da082a5666522fa9304653e52cbf08
Navnit Kapadia <navnitkapadia@gmail.com>
2022-01-06 10:22:25 +0530

    Create example.js
    Added javascript code

---
+++ example.js
@@ -0,0 +1,4 @@
+const ourVision = ["B$u$i$ld", "$t$$h$e", "N$e$x$t", "E$$ra", "$$o$f$", "S$$of$t$wa$r$e", "De$$ve$l$op$me$n$t"]
+
+const slogan = ourVision.map((text)=> text.split('$').join('').toUpperCase()).join(' ')
+console.log(slogan)
